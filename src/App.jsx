1277ced445499d3d307ffd71fac4b5cb9a426d3f
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-05-11 17:50:36 +0200

    recuperato json tramite fetch in useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
+
+const storeUrl = 'https://fakestoreapi.com/products';
+
+//esempio oggetto prodotto {"id":1,
+// "title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+// "price":109.95,
+// "description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+// "category":"men's clothing",
+// "image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+// "rating":{"rate":3.9,"count":120}}
 
 function App() {
-  return <button className='btn btn-primary'>click</button>;
+  const [productList, setProductList] = useState([])
+
+  useEffect(() => {
+    fetch(storeUrl)
+    .then(resp => resp.json())
+    .then(json => setProductList(json))
+    .catch(error => `Errore nel download dei dati, ${error}`)
+    
+  }, [])
+
+
+  return <>
+    <button className='btn btn-primary'>click</button>
+    <div>{JSON.stringify(productList)}</div>
+  </>;
 }
 
 export default App
